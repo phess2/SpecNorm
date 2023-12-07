01b6d936969156d4383d886b69a695af733ce525
Robert P Hess <rphess@ranvier.ib.cluster>
2023-12-06 23:32:44 -0500

    Add norm capabilities

--- src/classifiers.py
+++ src/classifiers.py
@@ -1,4 +1,4 @@
-# source: https://github.com/LJY-HY/cifar_pytorch-lightning/blob/master/models/classifiers.py
+# modified from: https://github.com/LJY-HY/cifar_pytorch-lightning/blob/master/models/classifiers.py
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -17,24 +17,45 @@ class CIFAR100_LIGHTNING(pl.LightningModule):
     def __init__(self, norm):
         super(CIFAR100_LIGHTNING, self).__init__()
         self.norm_type = norm
+        if self.norm_type == 'frob':
+            self.target_norms = self.get_targert_frob_norms()
+            self.automatic_optimization = False
+        elif self.norm_type == 'spec':
+            self.target_norms = self.get_target_spec_norms()
+            self.automatic_optimization = False
+        else:
+            self.target_norms = None
 
     def forward(self, x):
         output = self.model(x)
         return output
 
-    def training_step(self,batch,batch_idx):
+    def training_step(self, batch, batch_idx):
         data, target = batch
         output = self.forward(data)
-        loss = F.cross_entropy(output,target)
+        if self.automatic_optimization == False:
+            opt = self.optimizers()
+            loss = F.cross_entropy(output,target)
+            opt.zero_grad()
+            self.manual_backward(loss)
+            opt.step()
+            if self.norm == 'frob':
+                for i, p in enumerate(self.parameters()):
+                    p.data = p.data / torch.norm(p.data, p='fro') * self.target_norms[i]
+            else:
+                for i, p in enumerate(self.parameters()):
+                    p.data = p.data / self.spectral_norm(p) * self.target_norms[i]
+        else:
+            loss = F.cross_entropy(output,target)
         tensorboard_logs = {'train_loss':loss}
         return {'loss':loss, 'log':tensorboard_logs}
 
     def configure_optimizers(self):
-        optimizer = optim.SGD(self.parameters(), lr=1e-1, momentum=0.9, weight_decay=5e-4)
+        optimizer = optim.Adam(self.parameters(), lr=0.01)
         lr_scheduler = {'scheduler': torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[60,120], gamma=0.1), 'interval': 'epoch'}
         return [optimizer], [lr_scheduler]
 
-    def validation_step(self,batch,batch_idx):
+    def validation_step(self, batch, batch_idx):
         data, target = batch
         output = self.forward(data)
         loss = F.cross_entropy(output,target)
@@ -42,14 +63,14 @@ class CIFAR100_LIGHTNING(pl.LightningModule):
         correct = pred.eq(target.view_as(pred)).sum().item()
         return {'val_loss':loss,'correct':correct}
 
-    def validation_epoch_end(self,outputs):
+    def validation_epoch_end(self, outputs):
         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
         sum_correct = sum([x['correct'] for x in outputs])
         tensorboard_logs = {'val_loss':avg_loss}
         print('Validation accuracy : ',sum_correct/10000,'\n\n')
         return {'avg_val_loss':avg_loss, 'log':tensorboard_logs}    
 
-    def test_step(self,batch,batch_idx):
+    def test_step(self, batch, batch_idx):
         data, target = batch
         output = self.forward(data)
         pred = output.argmax(dim=1,keepdim=True)
@@ -62,6 +83,20 @@ class CIFAR100_LIGHTNING(pl.LightningModule):
         tensorboard_logs = {'test_loss': avg_loss}
         print('Test accuracy :',sum_correct/10000,'\n')
         return {'avg_test_loss': avg_loss, 'log': tensorboard_logs}
+
+    def get_target_frob_norms(self):
+        return [torch.norm(w, p='fro') for w in self.parameters()]
+
+    def spectral_norm(self, w, n_steps=10):
+        v = torch.randn(w.shape[1], device=w.device)
+        for _ in range(n_steps):
+            v /= v.norm()
+            v = w @ v @ w
+        return v.norm().sqrt()
+
+    def get_target_spec_norms(self):
+        return [self.spectral_norm(w) for w in self.parameters()]
+
 
 class CIFAR100_Resnet(CIFAR100_LIGHTNING):
     # This Module is based on Resnet for dataset CIFAR100
@@ -80,10 +115,26 @@ class CIFAR100_Resnet(CIFAR100_LIGHTNING):
         del self.model.maxpool
         self.model.maxpool = lambda x : x
 
+
 class CIFAR100_MLP(CIFAR100_LIGHTNING):
     # This Module is based on MLP for dataset CIFAR100
-    def __init__(self, depth, width, norm):
+    def __init__(self, num_layers, width, norm):
         super(CIFAR100_MLP, self).__init__(norm)
-        self.depth = depth
+        self.num_layers = num_layers
         self.width = width
-        
+        self.layer_sizes = [3 * 32 * 32, width]
+
+        net = [nn.flatten(), nn.Linear(self.layer_sizes[0], self.width), nn.ReLU()]
+
+        for i in range(num_layers -2):
+            self.layer_sizes.append(width)
+            net.append([nn.Linear(width, width), nn.ReLU()])
+
+        self.layer_sizes.append(100)
+        net.append(nn.Linear(width, 100))
+        self.net = nn.Sequential(*net)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        output = self.net(x)
+        return self.softmax(output)
--- train.py
+++ train.py
@@ -8,38 +8,14 @@ import json
 import socket
 import os
 
-from pytorch_lightning import Trainer
+from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.plugins import DDPPlugin
 from src.classifiers import *
 from corpus.CIFAR100_dataset import *
 
 hostname = socket.gethostname()
-
-
-# if __name__ == '__main__':
-#     datasets = ['CIFAR10','CIFAR100']
-#     NNModels = ['VGG','Resnet','WideResnet','Densenet_BC','Densenet']
-#     for dataset in datasets:
-#         if dataset == 'CIFAR10':
-#             dm = CIFAR10DataModule()
-#             max_epochs = 60
-#         elif dataset == 'CIFAR100':
-#             dm = CIFAR100DataModule()
-#             max_epochs = 180
-#         for NNModel in NNModels:
-#             model_name = dataset + '_' + NNModel
-#             model = globals()[model_name]()
-#             modelpath  = './workspace/model_ckpts/' + model_name + '/'
-#             os.makedirs(modelpath, exist_ok=True)
-#             checkpoint_callback=ModelCheckpoint(filepath=modelpath)
-#             trainer=Trainer(checkpoint_callback=checkpoint_callback, gpus=1, num_nodes=1, max_epochs = max_epochs)
-#             if os.path.isfile(modelpath + 'final.ckpt'):
-#                 model = model.load_from_checkpoint(checkpoint_path=modelpath + 'final.ckpt')
-#             else:
-#                 trainer.fit(model, dm)
-#                 trainer.save_checkpoint(modelpath + 'final.ckpt')
-#             trainer.test(model, datamodule = dm)
+seed_everything(1)
 
 
 def run_train(args):
@@ -73,68 +49,29 @@ def run_train(args):
     else:
         ckpt_path = None
 
-    callbacks = []
+    dm = CIFAR100DataModule()
 
-    if isinstance(config['val_metric'], dict):
-        for name, value in config['val_metric'].items():
-            callbacks.append(ModelCheckpoint(
-                checkpoint_dir,
-                filename="{epoch}-{step}-best_"+name,
-                monitor=value,
-                mode="max",
-                save_top_k=1,
-#                 save_weights_only=True,
-                verbose=True,
-            ))
-    
+    if config['model_type'] == 'ResNet':
+        model = CIFAR100_Resnet(config['model_size'], config['norm'])
     else:
-        callbacks.append(ModelCheckpoint(
-            checkpoint_dir,
-            monitor=f"{config['val_metric']}",
-            mode="max" if 'ACC' in config['val_metric'] else "min",
-            save_top_k=1,
-            save_weights_only=True,
-            verbose=True,
-        ))
-
-    train_checkpoint = ModelCheckpoint(
-        checkpoint_dir,
-        monitor="Losses/train_loss",
-        mode="min",
-        save_top_k=1,
-        save_weights_only=True,
-        verbose=True,
-    )
-
-    callbacks.append(train_checkpoint)
-
-    trainer = Trainer(
-        default_root_dir=args.exp_dir,
-        max_epochs=config['hparas']['epochs'],
-
-        # log_every_n_steps = 10,
-        detect_anomaly=True,
-        num_nodes=args.num_nodes,
-        gpus=args.gpus,
-        accelerator="gpu" if args.gpus > 0 else 'cpu',
-        # resume_from_checkpoint = ckpt_path,  
-        strategy=DDPPlugin(find_unused_parameters=False),
-        val_check_interval=config['hparas']['valid_step'],
-        gradient_clip_val=config['hparas']['gradient_clip_val'],
-        profiler=None,
-        callbacks=callbacks)
-
-    module = AttentionalTrackingModule
-
-    if args.resume_training or ckpt_path:
-        if ckpt_path is None:
-            ckpt_path = sorted(checkpoint_dir.glob("*.ckpt"))[-1]
-        model = module.load_from_checkpoint(checkpoint_path=ckpt_path, config=config)  
+        model = CIFAR100_MLP(config['num_layers'], config['width'], config['norm'])
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    checkpoint_callback = ModelCheckpoint(filepath=checkpoint_dir)
+    trainer = Trainer(default_root_dir=args.exp_dir, 
+                      checkpoint_callback=checkpoint_callback, 
+                      gpus=args.gpus, 
+                      num_nodes=args.num_nodes, 
+                      max_epochs=config['hparas']['epochs'],
+                      detect_anomaly=True,
+                      accelerator="gpu" if args.gpus > 0 else 'cpu',
+                      strategy=DDPPlugin(find_unused_parameters=False)
+                      )
+    if ckpt_path is not None:
+        model = model.load_from_checkpoint(checkpoint_path=ckpt_path)
     else:
-        model = module(config)
-    trainer.fit(model)
-
-    trainer.fit(model)
+        trainer.fit(model, dm)
+        trainer.save_checkpoint(checkpoint_dir + 'final.ckpt')
+    trainer.test(model, datamodule = dm)
 
 
 def cli_main():
@@ -157,12 +94,6 @@ def cli_main():
         default=1,
         type=int,
         help="Number of nodes to use for training. (Default: 1)",
-    )
-    parser.add_argument(
-        "--mixed_precision",
-        default=True,
-        action='store_true',
-        help="Use 16 bit precision in training. (Default: False)",
     )
     parser.add_argument(
         "--gpus",
